292acf97ea9d255477511d6d6c2e5892ec541840
Smart developer <developer@yandex.ru>
2023-02-04 17:32:05 +0300

    first

--- main.py
+++ main.py
@@ -7,74 +7,9 @@ app = Flask(__name__)
 def main():
     return "<h1>Миссия Колонизация Марса</h1>"
 
-
 @app.route('/index')
 def index():
     return "<h1>И на Марсе будут яблони цвести!</h1>"
-
-
-@app.route('/promotion')
-def promotion():
-    a = ["Человечество вырастает из детства.</p>",
-         "Человечеству мала одна планета.</p>",
-         "Мы сделаем обитаемыми безжизненные пока планеты.</p>",
-         "И начнем с Марса!</p>",
-         "Присоединяйся!</p>"]
-    return f"""<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <title>Рекламная кампания</title>
-                  </head>
-                  <body>
-                    <h2>{'<p>'.join(a)}</h2>
-                  </body>
-                </html>"""
-
-
-@app.route('/image_mars')
-def image_mars():
-    return f"""<!doctype html>
-                    <html lang="en">
-                      <head>
-                        <meta charset="utf-8">
-                        <title>Привет, Марс!</title>
-                      </head>
-                      <body>
-                        <h1>Жди нас, Марс!</h1>
-                        <img src="{url_for('static', filename='img/mars.png')}" 
-                            alt="здесь должна была быть картинка, но не нашлась">
-                        <p>Вот она какая, красная планета.</p>
-                      </body>
-                    </html>"""
-
-
-@app.route('/promotion_image')
-def promotion_image():
-    return f"""<!doctype html>
-                    <html lang="en">
-                      <head>
-                        <meta charset="utf-8">
-                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                        <link rel="stylesheet"
-                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                        crossorigin="anonymous">
-                        <title>Реклама с картинкой</title>
-                      </head>
-                      <body>
-                        <h1>Жди нас, Марс!</h1>
-                        <img src="{url_for('static', filename='img/mars.png')}" 
-                            width="300" height="300" 
-                            alt="здесь должна была быть картинка, но не нашлась">
-                        <div class="alert alert-secondary" role="alert">Человечество вырастает из детства.</div>
-                        <div class="alert alert-success" role="alert">Человечеству мала одна планета.</div>
-                        <div class="alert alert-secondary" role="alert">Мы сделаем обитаемыми безжизненные пока планеты.</div>
-                        <div class="alert alert-warning" role="alert">И начнем с Марса!</div>
-                        <div class="alert alert-danger" role="alert">Присоединяйся!</div>
-                      </body>
-                    </html>"""
-
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
