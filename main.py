1bb5e969b7eca46743df156a59f71d978f9f201b
MoonWolf2552 <iliyaklassgg@gmail.com>
2023-02-17 21:15:08 +0300

    add carousel

--- main.py
+++ main.py
@@ -123,6 +123,48 @@ def promotion_image():
                     </html>"""
 
 
+@app.route('/carousel')
+def carousel():
+    return f"""<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <link rel="stylesheet"
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+                            integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+                            crossorigin="anonymous">
+                            <title>Пейзажи Марса</title>
+                          </head>
+                          <body>
+                            <h1 align="center">Пейзажи Марса</h1>
+                            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="true">
+                             <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                  <img src="static/img/mars1.jpg" class="d-block w-100"
+                                  alt="здесь должна была быть картинка, но не нашлась">
+                                </div>
+                                <div class="carousel-item">
+                                  <img src="static/img/mars2.jpg" class="d-block w-100" 
+                                  alt="здесь должна была быть картинка, но не нашлась">
+                                </div>
+                                <div class="carousel-item">
+                                  <img src="static/img/mars3.jpg" class="d-block w-100"
+                                  alt="здесь должна была быть картинка, но не нашлась">
+                                </div>
+                              </div>
+                              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                              </button>
+                              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                              </button>
+                            </div>
+                          </body>
+                        </html>"""
+
+
 @app.route('/form_sample', methods=['POST', 'GET'])
 def form_sample():
     if request.method == 'GET':
